a4c88baa10f33e58621042b72d15fdd7bdea200a
Daniel W. Barowy <dbarowy@cs.umass.edu>
2014-04-19 10:16:16 -0400

    Added source url field.

--- src/main/scala/net/ettinsmoor/Bingerator.scala
+++ src/main/scala/net/ettinsmoor/Bingerator.scala
@@ -26,6 +26,7 @@ case class ImageResult(result_xml: Node) extends SearchResult {
   val id = (result_xml \ "content" \ "properties" \ "ID").text
   val title = (result_xml \ "content" \ "properties" \ "Title").text
   val media_url = (result_xml \ "content" \ "properties" \ "MediaUrl").text
+  val source_url = (result_xml \ "content" \ "properties" \ "SourceUrl").text
   val display_url = (result_xml \ "content" \ "properties" \ "DisplayUrl").text
   val content_type = (result_xml \ "content" \ "properties" \ "ContentType").text
   val file_size = (result_xml \ "content" \ "properties" \ "FileSize").text
