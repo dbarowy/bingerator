2144d500aa0bcfe1581b81c6412ad6c870a272b3
Daniel W. Barowy <dbarowy@cs.umass.edu>
2014-04-19 10:12:07 -0400

    Fix: Image search and web search have different schemas.

--- src/main/scala/net/ettinsmoor/Bingerator.scala
+++ src/main/scala/net/ettinsmoor/Bingerator.scala
@@ -40,31 +40,41 @@ class Bingerator(key: String, debug: Boolean) {
   def this(key: String) = this(key, false)
 
   def SearchWeb(query: String) : Stream[WebResult] = {
-    val urlf = _web_request_url(query)(_)
-    val newT = _new_web_result(_)
-    _search[WebResult](query, 0, urlf, newT)
+    val url_fn = _web_request_url(query)(_)
+    val new_fn = _new_web_result(_)
+    val parse_fn = _web_xml_parser(_)
+    _search[WebResult](query, 0, url_fn, new_fn, parse_fn)
   }
 
   def SearchImages(query: String) : Stream[ImageResult] = {
-    val urlf = _image_request_url(query)(_)
-    val newT = _new_image_result(_)
-    _search[ImageResult](query, 0, urlf, newT)
+    val url_fn = _image_request_url(query)(_)
+    val new_fn = _new_image_result(_)
+    val parse_fn = _image_xml_parser(_)
+    _search[ImageResult](query, 0, url_fn, new_fn, parse_fn)
   }
 
   private def _search[T <: SearchResult](query: String,
                                          offset: Int,
-                                         url_factory: Int => URL,
-                                         type_factory: Node => T) : Stream[T] = {
-    val result = _execute_search[T](query, offset, url_factory, type_factory)
+                                         url_generator: Int => URL,
+                                         type_instantiator: Node => T,
+                                         parser: Node => NodeSeq) : Stream[T] = {
+    val result = _execute_search[T](query, offset, url_generator, type_instantiator, parser)
     // if we get fewer than the expected number of results, do not recurse
-    if (result.length < _results_size) {
-      result.toStream
-    } else {
-      result.toStream #::: _search[T](query, offset + _results_size - 1, url_factory, type_factory)
+    def more : Stream[T] = {
+      if (result.length < _results_size) {
+        Stream.empty
+      } else {
+        _search[T](query, offset + _results_size, url_generator, type_instantiator, parser)
+      }
     }
+    result.toStream #::: more
   }
 
-  private def _execute_search[T <: SearchResult](query: String, offset: Int, url_factory: Int => URL, type_factory: Node => T) : Seq[T] = {
+  private def _execute_search[T <: SearchResult](query: String,
+                                                 offset: Int,
+                                                 url_generator: Int => URL,
+                                                 type_instantiator: Node => T,
+                                                 parser: Node => NodeSeq) : Seq[T] = {
     if (debug) println("[debug]: Query: \"" + query +
                        "\". Getting results " + offset +
                        " to " + (offset + _results_size - 1) + ".")
@@ -73,7 +83,7 @@ class Bingerator(key: String, debug: Boolean) {
     val buf = new StringBuilder()
 
     // issue query and get handle to results
-    val conn: URLConnection = url_factory(offset).openConnection()
+    val conn: URLConnection = url_generator(offset).openConnection()
     conn.setRequestProperty("Authorization", "Basic " + _key_b64)
     val reader: BufferedReader = new BufferedReader(new InputStreamReader(conn.getInputStream()))
 
@@ -94,12 +104,12 @@ class Bingerator(key: String, debug: Boolean) {
     val xmldata = XML.loadString(buf.toString())
 
     // get results
-    val res: NodeSeq = xmldata \\ "entry"
+    val res = parser(xmldata)
 
     if (debug) println("[debug]: Query: \"" + query + "\". Got " + res.size + " results.")
 
     // parse and return result objects
-    res.map (resxml => type_factory(resxml))
+    res.map (resxml => type_instantiator(resxml))
   }
 
   private def _new_web_result(xml: Node) = new WebResult(xml)
@@ -117,6 +127,10 @@ class Bingerator(key: String, debug: Boolean) {
       URLEncoder.encode(query, "UTF-8") +
       "%27&$top=" + _results_size +
       "&$skip=" + offset + "&$format=ATOM")
+
+  private def _web_xml_parser(xml: Node) = xml \\ "entry"
+
+  private def _image_xml_parser(xml: Node) = xml \\ "feed" \ "entry" \ "link" \ "inline" \ "feed" \ "entry"
 }
 
 object KeyFormat {
